6d67bacd9e23fc112cfcdabb9a228c2de3274586
JavaCommons Technologies <javacommons@gmail.com>
2021-10-09 04:45:42 +0900

    .

--- jsweet/run.sh
+++ jsweet/run.sh
@@ -1,3 +1,4 @@
 cd jsweet-live-sandbox.tmp
-google-chrome http://localhost:18580/ &
+#google-chrome http://localhost:18580/ &
+firefox http://localhost:18580/ &
 java -jar target/jsweet-live-sandbox-3.1.0-jar-with-dependencies.jar
