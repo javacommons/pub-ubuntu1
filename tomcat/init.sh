de822fec6ce1b573381f3728e99ef196199b945e
JavaCommons Technologies <javacommons@gmail.com>
2021-10-09 04:42:10 +0900

    .

--- tomcat/init.sh
+++ tomcat/init.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install -y openjdk-11-jdk gradle maven firefox firefox-locale-ja
+sudo apt install -y openjdk-11-jdk gradle maven firefox
 wget -nc https://ftp.riken.jp/net/apache/tomcat/tomcat-8/v8.5.71/bin/apache-tomcat-8.5.71.tar.gz
 rm -rf apache-tomcat-8.5.71
 tar xvf apache-tomcat-8.5.71.tar.gz
