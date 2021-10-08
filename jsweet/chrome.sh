a121b02bb6e974356c4a09cde23c2a906d45cdd2
JavaCommons Technologies <javacommons@gmail.com>
2021-10-09 02:32:02 +0900

    .

--- jsweet/chrome.sh
+++ jsweet/chrome.sh
@@ -1,4 +1,5 @@
-sudo apt install -y fonts-liberation
+sudo apt update
+sudo apt install -y gedit fonts-liberation
 cd /tmp
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
