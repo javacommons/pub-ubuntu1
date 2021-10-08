7af7006b5dd474f3771d88120c66d453a1ddfd29
JavaCommons Technologies <javacommons@gmail.com>
2021-10-09 01:54:56 +0900

    .

--- jsweet/deno.sh
+++ jsweet/deno.sh
@@ -1,1 +1,3 @@
+sudo apt update
+sudo apt install -y unzip
 curl -fsSL https://deno.land/x/install/install.sh | sh
--- jsweet/init.sh
+++ jsweet/init.sh
@@ -1,6 +1,7 @@
-curl -fsSL https://deno.land/x/install/install.sh | sh
-sudo apt install -y openjdk-11-jdk gradle maven nodejs npm
+sudo apt update
+sudo apt install -y openjdk-11-jdk gradle maven nodejs npm unzip
 rm -rf jsweet-live-sandbox.tmp
 git clone https://github.com/lgrignon/jsweet-live-sandbox jsweet-live-sandbox.tmp
 cd jsweet-live-sandbox.tmp
 mvn clean && mvn -P client generate-sources && mvn package assembly:single
+curl -fsSL https://deno.land/x/install/install.sh | sh
