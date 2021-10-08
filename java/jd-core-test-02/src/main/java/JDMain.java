4b3d6cc9063b51a721626ef2090dec7cd907dbbe
JavaCommons Technologies <javacommons@gmail.com>
2021-10-09 03:30:48 +0900

    .

---
+++ java/jd-core-test-02/src/main/java/JDMain.java
@@ -0,0 +1,7 @@
+import java.io.File;
+
+public class JDMain {
+	public static void main(String[] args) throws Exception {
+		ult.lib1.Main.investigate(new File("./build/libs/jd-core-test-02-0.0.2.jar"));
+	}
+}
