cbd402b92a1525ea98926cfb54fe15a0685308da
JavaCommons Technologies <javacommons@gmail.com>
2021-10-09 19:30:14 +0900

    .

--- qt.sh
+++ qt.sh
@@ -1,13 +1,4 @@
-sudo apt update && sudo apt upgrade -y
-#sudo apt install -y qtbase5-dev qttools5-dev-tools qt5-default
-#sudo apt install -y qtcreator
-#sudo apt install -y qt5-doc
-#sudo apt install -y qt5-doc-html qtbase5-doc-html
-#sudo apt install -y qtbase5-examples
-#sudo apt install -y qtdeclarative5-dev
-#sudo apt install -y qtquickcontrols5-examples qtquickcontrols2-5-examples
-#sudo apt install -y qtwebengine5-dev qtwebengine5-dev-tools qtwebengine5-doc qtwebengine5-examples
-
+#sudo apt update && sudo apt upgrade -y
 sudo apt-get install -y build-essential
 sudo apt-get install -y libfontconfig1
 sudo apt-get install -y mesa-common-dev
